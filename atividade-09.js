0b22136084bd78c10aa5982c6747123077e00a79
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-05-30 11:33:23 -0300

    Verifica a presença de um item no array

---
+++ atividade-09.js
@@ -0,0 +1,11 @@
+/*9 – Conferência de presença
+O RH quer saber se o funcionário "Amanda Souza" compareceu à reunião de alinhamento. Verifique essa informação na lista de presença. */
+
+let presenca = [
+    "João Lima",
+    "Carla Nunes",
+    "Amanda Souza",
+    "Bruno Ribeiro",
+    "Fernanda Sales"
+  ];
+  console.log(`A lista possui Amanda Souza? ${presenca.includes("Amanda Souza")}`);
