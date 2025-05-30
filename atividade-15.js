f9ccd1eacdeb8a8cfc3e8c0f26c47ce0c326b159
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-05-30 11:57:59 -0300

    Adiciona o arquivo atividade-15.js

---
+++ atividade-15.js
@@ -0,0 +1,14 @@
+/*15 – Remoção de itens inativos no menu
+Dois itens do menu de navegação estão obsoletos e precisam ser removidos da posição 2 e 3, pois o sistema não oferece mais esses serviços.*/
+
+let menuNavegacao = [
+    "Início",
+    "Serviços",
+    "Portfólio",
+    "Blog",
+    "Contato",
+    "Login",
+    "Área do cliente"
+  ];
+menuNavegacao.shift[2,3];
+console.log(`Menu com os serviços removidos ${menuNavegacao}`);
