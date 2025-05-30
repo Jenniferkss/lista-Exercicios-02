91421db23714724de94c5df6e1953f6d0396c374
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-05-30 11:40:23 -0300

    Tranforma o array em uma string

---
+++ atividade-12.js
@@ -0,0 +1,11 @@
+/*12 – Tags para URL
+Na construção dinâmica de URLs de busca, o array de palavras-chave precisa ser convertido em uma string separada por traços para funcionar corretamente. */
+
+let palavrasChave = [
+    "desenvolvimento",
+    "frontend",
+    "javascript",
+    "css",
+    "react"
+  ];
+console.log(`Lista como string: ${palavrasChave.join(`-`)}`);
