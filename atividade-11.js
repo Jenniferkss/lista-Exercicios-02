69f3ba8255ffb98a051ded57d6ae2ed1d6ea410a
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-05-30 11:38:32 -0300

    Tranforma o array em uma string

---
+++ atividade-11.js
@@ -0,0 +1,12 @@
+/*11 – Geração de frase
+Você precisa exibir os ingredientes de uma receita em uma única frase legível para o cliente. Utilize o array de ingredientes.*/
+
+let ingredientes = [
+    "farinha de trigo",
+    "fermento",
+    "leite",
+    "ovos",
+    "açúcar",
+    "manteiga"
+  ];
+  console.log(`Lista como string: ${ingredientes.join(`,`)}`);
