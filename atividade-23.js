210ef981ff59a4ab19117368dc3ed6dc9b7a3116
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-06-01 23:05:04 -0300

    feat:organiza o array em ordem alfabetica

---
+++ atividade-23.js
@@ -0,0 +1,15 @@
+/*23 – Organizar lista de candidatos
+A lista de candidatos para entrevista deve ser organizada em ordem alfabética antes de ser enviada ao RH.*/
+
+let candidatos = [
+  "Marcos",
+  "Aline",
+  "Carla",
+  "João",
+  "Fernanda",
+  "Lucas",
+  "Bruno",
+  "Isabela"
+];
+let listaOrdenada = candidatos.sort();
+console.log("Lista de candidatos organizada em ordem alfabetica",listaOrdenada);
